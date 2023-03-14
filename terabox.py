65e0ef1cfca28b9b7d3ba95dccc8d1f07decbc3b
Irfan Ammar Afif <ipangcheater@gmail.com>
2023-03-14 16:10:37 +0700

    Update terabox.py
    - fix downloading from videoPlay url like from shared folder
    - fix output filename
    - update user-agent

--- terabox.py
+++ terabox.py
@@ -28,21 +28,29 @@ print('Cookies Parsed')
 
 inp = input('Enter the Link: ')
 dom = inp.split("/")[2]
+typ = inp.split("/")[-1].split("?")[0]
 fxl = inp.split("=")
-key = fxl[-1]
 
-URL = f'https://{dom}/share/list?app_id=250528&shorturl={key}&root=1'
+if typ == "link":
+    key = fxl[-1]
+    URL = f'https://{dom}/share/list?app_id=250528&shorturl={key}&root=1'
+elif typ == "videoPlay":
+    key = fxl[1].split("&")[0]
+    dir = fxl[2].split("&")[0]
+    name = fxl[4]
+    URL = f'https://{dom}/share/list?app_id=250528&shorturl={key}&path={dir}%2F{name}&root=1'
 
 header = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': f'https://{dom}/sharing/link?surl={key}',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+    'Referer': f'https://{dom}/sharing/{typ}?surl={key}',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:110.0) AppleWebKit/605.1.15 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/605.1.15'
 }
 
-resp = requests.get(url=URL, headers=header, cookies=cookies).json()[
-    'list'][0]['dlink']
+resp = requests.get(url=URL, headers=header, cookies=cookies).json()['list'][0]
+filename = resp['server_filename']
+dlink = resp['dlink']
     
-subprocess.run([aria2c, '--console-log-level=warn', '-x 16',
-               '-s 16', '-j 16', '-k 1M', '--file-allocation=none', resp])
+subprocess.run([aria2c,'-o' + filename, '--console-log-level=warn', '-x 16',
+               '-s 16', '-j 16', '-k 1M', '--file-allocation=none', dlink])
